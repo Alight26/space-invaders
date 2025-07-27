fb78a18c636f7a846300b8ea9c20461b7b634876
Lucas <luc.coulson6@gmail.com>
2025-07-27 10:14:37 -0400

    added playable area

--- main.py
+++ main.py
@@ -8,13 +8,31 @@ screen = pygame.display.set_mode((800, 600))
 #Title and Icon
 pygame.display.set_caption("Space Invaders")
 
+
+# Player
+playerimg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x, y):
+    #blit means draw
+    screen.blit(playerimg, (x, y))
+
 # game loop
 running = True
 while running:
+    #background screen color
+    screen.fill((0, 0, 0))
+
+    # to exit the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    #background screen color
-    screen.fill((255, 0, 0))
+    # keyboard movement of the character
+
+
+
+    player(playerX, playerY)
     pygame.display.update()
