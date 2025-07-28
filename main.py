94fe248783d02bc3cc44a37163c426afe6f56334
Lucas <luc.coulson6@gmail.com>
2025-07-28 17:43:23 -0400

    can now fire a bullet

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ enemyY = random.randint(50, 150)
 enemyX_change = 0.3
 enemyY_change = 40
 
+
+# Bullet 
+
+# "Ready Stste" - You cna't see the bullet on the screen
+# "Fire State" - The Bullet is moving
+
+bulletimg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 1
+bullet_state = "ready"
+
 def player(x, y):
     #blit means draw
     screen.blit(playerimg, (x, y))
@@ -30,6 +43,11 @@ def player(x, y):
 def enemy(x, y):
     #blit means draw
     screen.blit(enemyimg, (x, y))
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletimg, (x + 16, y + 10))
 
 # game loop
 running = True
@@ -51,11 +69,15 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change += 0.3
                 print("Right arrow is pressed")
+            # shooting
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
                 print("Keystroke has been released")
+
 
 
     playerX += playerX_change
@@ -76,7 +98,12 @@ while running:
         enemyY += enemyY_change
     elif enemyX >= 736:
         enemyX_change = - 0.2
-        enemyY += enemyY_change
+        enemyY -= enemyY_change
+
+    # Bullet Movement
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
